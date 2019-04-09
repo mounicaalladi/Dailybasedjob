022c1867c442bae1dd4db5b1e1edd9579a5d24ee
sai manikanta <sai.manikanta56@gmail.com>
2019-04-09 12:52:04 +0530

    pro

--- Dailybasedjobs/Candidateprofile.aspx.cs
+++ Dailybasedjobs/Candidateprofile.aspx.cs
@@ -4,14 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace Dailybasedjobs
 {
     public partial class Candidateprofile : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Dailybasedjob"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into CandidateProfile(Name,Skills,Languages,Age,CurrentSalary,ExpectedSalary,Phone,Email,Pincode,Area,Address) values('" + txtyn.Text + "','" + txtsk.Text + "','" + txtlan.Text + "','" + txtage.Text + "','" + txtcs.Text + "','"+txtes.Text+"','"+Txtphone.Text+"','"+txtemail.Text+"','"+Txtpin.Text+"','"+Txtarea.Text+"','"+Txtaddress.Text+"')", con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
     }
 }
