5d9c8ab2bde7db1cc75b11e91c6ef25aaa39815e
sai manikanta <sai.manikanta56@gmail.com>
2019-04-09 08:43:09 +0530

    register

--- Dailybasedjobs/Register.aspx.cs
+++ Dailybasedjobs/Register.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Dailybasedjobs
 {
-    public partial class Admin : System.Web.UI.MasterPage
+    public partial class Register : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
